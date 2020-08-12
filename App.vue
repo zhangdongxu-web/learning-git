cfb28393c4ccb5051e0f719947ed41baf8ffb00e
yourName <527812348@qq.com>
2020-08-12 14:58:11 +0800

    uni-app-dome

---
+++ App.vue
@@ -0,0 +1,18 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+      // uni.hideTabBar();   // 隐藏原声  navbar-
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
