a65ee7e2d4719e3f491d0129ac7247fdd2971477
yourName <527812348@qq.com>
2020-08-12 13:47:43 +0800

    uni-app-dome

---
+++ gbd/components/nav-bar/nav-bar.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="footer-nav">
+	  <view class="item" @click = "goaaa">
+	    <text>首页</text>
+	  </view>
+    <view class="item" @click = "gobbb">
+      <text>二页</text>
+    </view>
+    <view class="item" @click = "goccc">
+      <text>三页</text>
+    </view>
+    <view class="item" @click = 'goddd'>
+      <text>四页</text>
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		computed: {
+			
+		},
+		props: {
+			
+		},
+		methods: {
+      goaaa() {
+        // console.log(888)
+        uni.switchTab({
+          url: '../../pages/index/index',
+          // success: res => {},
+          // fail: () => {},
+          // complete: () => {}
+        });
+      },
+      gobbb() {
+        // console.log(888)
+        uni.switchTab({
+          url: '../../pages/yingbinlu/yingbinlu',
+          // success: res => {},
+          // fail: () => {},
+          // complete: () => {}
+        });
+      },
+      goccc() {
+        uni.switchTab({
+          url: '../../pages/tuanjielu/tuanjielu',
+          // success: res => {},
+          // fail: () => {},
+          // complete: () => {}
+        });
+      },
+      goddd() {
+        uni.navigateTo({
+          url: '../../pages/aaa/aaa',
+          success: res => {},
+          fail: () => {},
+          complete: () => {}
+        });
+      }
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.footer-nav {
+    border-radius:30rpx;
+		position: fixed;
+		width: 80%;
+		height: 133upx;
+		bottom: 30rpx;
+		left: 10%;
+		z-index: 99;
+    background:pink;
+    opacity:0.7;
+		// background: url('../../static/logo.png') no-repeat center;
+		// background-size: cover;
+		display: flex;
+		justify-content: space-around;
+    align-items:center;
+    .item {
+      box-sizing: border-box;
+      width:100rpx;
+      height:100rpx;
+      border-radius:50%;
+      background:red;
+      display:flex;
+      flex-direction:column;
+      justify-content:flex-end;
+      padding-bottom:10rpx;
+      text {
+        
+        text-align:center;
+        // margin: auto;
+        
+      }
+    }
+		
+	}
+</style>
---
+++ gbd/pages/tuanjielu/tuanjielu.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="content">
+		  <view class="lu">
+		    
+		  </view>
+      <view class="luz">
+       <button type="default" @click="gobottom">go ↓ bottom</button> 
+       <button type="default" @click="gotop">go ↑ top</button>
+      </view>
+      <view class="lu">
+        
+      </view>
+		</view>
+    <nav-bar :navlist='navlist' :sort='sort'></nav-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+    onLoad() {
+      
+    },
+		methods: {
+			gotop() {
+        wx.pageScrollTo({
+          scrollTop:0,
+          duration:3000
+        })
+      },
+      gobottom() {
+        wx.pageScrollTo({
+          scrollTop:2900,
+          duration:3000
+        })
+      }
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+.content {
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  .lu {
+    width:150rpx;
+    height:2900rpx;
+    background: linear-gradient(to bottom, #f00 0%, #000 100%);
+  }
+  .luz {
+    box-sizing:border-box;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    width:300rpx;
+    height:2900rpx;
+    padding:200rpx 0;
+    background: linear-gradient(to bottom, #000 0%, #f00 100%);
+    button {
+      opacity:0.3;
+    }
+  }
+  
+}
+</style>
